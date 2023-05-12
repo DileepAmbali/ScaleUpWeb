e489ab3dc9a0ac80d8c574977dd185d82d061abf
Dileep Ambali <ambalidileep@gmail.com>
2023-05-12 17:26:08 +0530

    Added Cart and Order route

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
+const orderRoute = require("./routes/order")
+const cartRoute = require("./routes/cart")
 
 dotenv.config()
 app.use(express.json())
@@ -19,6 +21,8 @@ mongoose
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
 app.use("/api/products", productRoute)
+app.use("/api/orders", orderRoute)
+app.use("/api/carts", cartRoute)
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log("Server has started on port 5000")
