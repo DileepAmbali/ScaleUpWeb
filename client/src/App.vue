60e01894e9e66acfce4d1fc49c806027cbce5eb2
Dileep Ambali <ambalidileep@gmail.com>
2023-05-16 10:56:11 +0530

    Google login added for future use.

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,12 +2,22 @@
 import { RouterView, RouterLink } from "vue-router"
 
 import HomeView from "./views/HomeView.vue"
+import Navbar from "./components/Navbar.vue"
+import Footer from "./components/Footer.vue"
+import { decodeCredential } from "vue3-google-login"
+const callback = (response) => {
+	// decodeCredential will retrive the JWT payload from the credential
+	const userData = decodeCredential(response.credential)
+	console.log("Handle the userData", userData)
+}
 </script>
 
 <template>
-	<div>
-		<RouterView />
-	</div>
+	<Navbar />
+
+	<RouterView />
+
+	<Footer />
 </template>
 
 <style scoped></style>
