4070ab45a0e08de334c8a6d8ea772e25374510ae
Dileep Ambali <ambalidileep@gmail.com>
2023-05-16 10:49:04 +0530

    Main Js file updated

--- client/src/main.js
+++ client/src/main.js
@@ -10,11 +10,22 @@ import PrimeVue from "primevue/config"
 
 import App from "./App.vue"
 import router from "./router"
+import vue3GoogleLogin from "vue3-google-login"
+
+// !Dead Code
+import "vuetify/styles"
+import { createVuetify } from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue)
+app.use(vue3GoogleLogin, {
+	clientId:
+		"324515309378-m9e6agbnd1ghre1m8emgfp6eh377hvdk.apps.googleusercontent.com"
+})
 
 app.mount("#app")
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,8 +7,109 @@ const router = createRouter({
 		{
 			path: "/",
 			name: "home",
-			component: HomeView,
+			component: HomeView
 		},
+		{
+			path: "/login",
+			name: "login",
+			component: () => import("../views/LoginView.vue")
+		},
+		{
+			path: "/register",
+			name: "register",
+			component: () => import("../views/RegisterView.vue")
+		}
+		// {
+		// 	path: "/products",
+		// 	name: "products",
+		// 	component: () => import("../views/ProductsView.vue")
+		// },
+		// {
+		// 	path: "/product/:id",
+		// 	name: "product",
+		// 	component: () => import("../views/ProductView.vue")
+		// },
+		// {
+		// 	path: "/cart",
+		// 	name: "cart",
+		// 	component: () => import("../views/CartView.vue")
+		// },
+		// {
+		// 	path: "/success",
+		// 	name: "success",
+		// 	component: () => import("../views/SuccessView.vue")
+		// },
+		// {
+		// 	path: "/admin",
+		// 	name: "admin",
+		// 	component: () => import("../views/AdminView.vue")
+		// },
+		// {
+		// 	path: "/admin/products",
+		// 	name: "admin-products",
+		// 	component: () => import("../views/AdminProductsView.vue")
+		// },
+		// {
+		// 	path: "/admin/product/:id",
+		// 	name: "admin-product",
+		// 	component: () => import("../views/AdminProductView.vue")
+		// },
+		// {
+		// 	path: "/admin/users",
+		// 	name: "admin-users",
+		// 	component: () => import("../views/AdminUsersView.vue")
+		// },
+		// {
+		// 	path: "/admin/user/:id",
+		// 	name: "admin-user",
+		// 	component: () => import("../views/AdminUserView.vue")
+		// },
+		// {
+		// 	path: "/admin/orders",
+		// 	name: "admin-orders",
+		// 	component: () => import("../views/AdminOrdersView.vue")
+		// },
+		// {
+		// 	path: "/admin/order/:id",
+		// 	name: "admin-order",
+		// 	component: () => import("../views/AdminOrderView.vue")
+		// },
+		// {
+		// 	path: "/admin/create",
+		// 	name: "admin-create",
+		// 	component: () => import("../views/AdminCreateView.vue")
+		// },
+		// {
+		// 	path: "/admin/create-product",
+		// 	name: "admin-create-product",
+		// 	component: () => import("../views/AdminCreateProductView.vue")
+		// },
+		// {
+		// 	path: "/admin/create-user",
+		// 	name: "admin-create-user",
+		// 	component: () => import("../views/AdminCreateUserView.vue")
+		// },
+		// {
+		// 	path: "/admin/create-order",
+		// 	name: "admin-create-order",
+		// 	component: () => import("../views/AdminCreateOrderView.vue")
+		// },
+		// {
+		// 	path: "/admin/edit/:id",
+		// 	name: "admin-edit",
+		// 	component: () => import("../views/AdminEditView.vue")
+		// },
+		// {
+		// 	path: "/admin/edit-product/:id",
+		// 	name: "admin-edit-product",
+		// 	component: () => import("../views/AdminEditProductView.vue")
+		// },
+		// {
+		// 	path: "/admin/edit-user/:id",
+		// 	name: "admin-edit-user",
+		// 	component: () => import("../views/AdminEditUserView.vue")
+		// }
+
 		// {
 		//   path: '/about',
 		//   name: 'about',
@@ -17,7 +118,7 @@ const router = createRouter({
 		//   // which is lazy-loaded when the route is visited.
 		//   component: () => import('../views/AboutView.vue')
 		// }
-	],
+	]
 })
 
 export default router
