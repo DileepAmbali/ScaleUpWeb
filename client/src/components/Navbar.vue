e8c8a0b4af6d45f5216f89b0cf667ddf0ae2eb85
Dileep Ambali <ambalidileep@gmail.com>
2023-06-16 18:35:33 +0530

    Removed Flowbite
    Damn

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,117 +1,118 @@
 <template>
-  <nav class="bg-gray-50 border-gray-200 dark:bg-gray-900">
-    <div
-      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-6 py-6 md:p-8"
-    >
-      <div>
-        <a href="/" class="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8 w-8 mr-3"
-            alt="Flowbite Logo"
-          />
-          <span
-            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-            >ScaleUpWeb</span
-          >
-        </a>
-      </div>
-      <div class="flex items-center md:order-2">
-        <!-- User Dropdown -->
-        <RouterLink
-          :to="{ name: 'cart', params: { customerId: id } }"
-          class="active:dark:bg-gray-950 active:bg-slate-300 rounded-xl"
-        >
-          <CartSideMenu />
-          <ShoppingCartIcon class="w-7 h-7 m-2 mx-6 dark:text-white" />
-        </RouterLink>
-        <UserDropdown class="hidden md:flex" />
-        <!--TODO :logout-route="logoutRoute" -->
-        <!-- User Dropdown Ends -->
+	<nav class="bg-gray-50 border-gray-200 dark:bg-gray-900">
+		<div
+			class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-6 py-6 md:p-8"
+		>
+			<div>
+				<a href="/" class="flex items-center">
+					<img
+						src="https://flowbite.com/docs/images/logo.svg"
+						class="h-8 w-8 mr-3"
+						alt="Flowbite Logo"
+					/>
+					<span
+						class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+						>ScaleUpWeb</span
+					>
+				</a>
+			</div>
+			<div class="flex items-center md:order-2">
+				<!-- User Dropdown -->
+				<RouterLink
+					:to="{ name: 'cart', params: { customerId: id } }"
+					class="active:dark:bg-gray-950 active:bg-slate-300 rounded-xl"
+				>
+					<CartSideMenu />
+					<ShoppingCartIcon
+						class="w-7 h-7 m-2 mx-6 dark:text-white"
+					/>
+				</RouterLink>
+				<UserDropdown class="hidden md:flex" />
+				<!--TODO :logout-route="logoutRoute" -->
+				<!-- User Dropdown Ends -->
 
-        <!-- Hamburger -->
-        <button
-          data-collapse-toggle="mobile-menu-2"
-          type="button"
-          class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="mobile-menu-2"
-          aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <!-- Hamburger -->
-      </div>
-      <div
-        class="items-center justify-between hidden w-full md:flex md:w-auto"
-        id="mobile-menu-2"
-      >
-        <div class="flex gap-4">
-          <div class="w-1/2 ms:w-2/3 md:w-full">
-            <ul
-              class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-inherit shadow-sm md:shadow-none md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
-            >
-              <li>
-                <a
-                  href="/"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  aria-current="page"
-                  >Home</a
-                >
-              </li>
-              <li>
-                <a
-                  href="/products"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >Products</a
-                >
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >Shops</a
-                >
-              </li>
-            </ul>
-          </div>
-          <UserDropDownMenu class="mt-4 md:hidden" />
-        </div>
-      </div>
-    </div>
-  </nav>
+				<!-- Hamburger -->
+				<button
+					data-collapse-toggle="mobile-menu-2"
+					type="button"
+					class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+					aria-controls="mobile-menu-2"
+					aria-expanded="false"
+				>
+					<span class="sr-only">Open main menu</span>
+					<svg
+						class="w-6 h-6"
+						aria-hidden="true"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+							clip-rule="evenodd"
+						></path>
+					</svg>
+				</button>
+				<!-- Hamburger -->
+			</div>
+			<div
+				class="items-center justify-between hidden w-full md:flex md:w-auto"
+				id="mobile-menu-2"
+			>
+				<div class="flex gap-4">
+					<div class="w-1/2 ms:w-2/3 md:w-full">
+						<ul
+							class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-inherit shadow-sm md:shadow-none md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+						>
+							<li>
+								<a
+									href="/"
+									class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+									aria-current="page"
+									>Home</a
+								>
+							</li>
+							<li>
+								<a
+									href="/products"
+									class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+									>Products</a
+								>
+							</li>
+							<li>
+								<a
+									href="#"
+									class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+									>Shops</a
+								>
+							</li>
+						</ul>
+					</div>
+					<UserDropDownMenu class="mt-4 md:hidden" />
+				</div>
+			</div>
+		</div>
+	</nav>
 </template>
 
 <script setup>
-import { Dropdown } from "flowbite-vue";
-import { computed, ref } from "vue";
-import { RouterLink, useRoute, useRouter } from "vue-router";
-import { ShoppingCartIcon } from "@heroicons/vue/24/outline";
-import CartSideMenu from "./CartSideMenu.vue";
-import UserDropdown from "./UserDropDown.vue";
-import UserDropDownMenu from "./UserDropDownMenu.vue";
+import { computed, ref } from "vue"
+import { RouterLink, useRoute, useRouter } from "vue-router"
+import { ShoppingCartIcon } from "@heroicons/vue/24/outline"
+import CartSideMenu from "./CartSideMenu.vue"
+import UserDropdown from "./UserDropDown.vue"
+import UserDropDownMenu from "./UserDropDownMenu.vue"
 
-const router = useRouter();
+const router = useRouter()
 
-const id = ref("1"); //This should be the id of the user logged in
+const id = ref("1") //This should be the id of the user logged in
 
-const usersName = "John Doe";
-const usersEmailId = "johndoe@gmail.com";
+const usersName = "John Doe"
+const usersEmailId = "johndoe@gmail.com"
 
-const isCustomer = true;
-const isMerchant = false;
+const isCustomer = true
+const isMerchant = false
 </script>
 
 <style scoped></style>
